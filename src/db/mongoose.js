44705d3d28b7ee8078cfa012bbc4980c60f760fd
mezzanine510 <mezzanine510@gmail.com>
2019-05-22 13:08:28 -0700

    added basic user creation and error status message

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,63 +1,15 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
 
-// Users
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error(`The password can't include the word "${value}"`);
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number.');
-            }
-        }
-    }
-});
-
-const me = new User({
-    name: 'Bushanote  ',
-    email: 'poopanote@rawr.com',
-    password: 'passWoRd'
-});
-
-// me.save().then((result, reject) => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log('Error!', error);
+// const me = new User({
+//     name: 'Bushanote  ',
+//     email: 'poopanote@rawr.com',
+//     password: 'passWoRd'
 // });
-
 
 // Tasks
 const Task = mongoose.model('Task', {
@@ -70,15 +22,4 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         default: false
     }
-});
-
-const myTask = new Task({
-    description: 'be berry friendlies',
-    completed: true
-});
-
-myTask.save().then((result, reject) => {
-    console.log(myTask);
-}).catch((error) => {
-    console.log('Error!', error);
 });
