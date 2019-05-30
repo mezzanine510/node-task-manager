fca5276cdf51a5e483bff2bdffcda139628c6fd2
mezz <mezzanine510@gmail.com>
2019-05-29 20:09:02 -0700

    added DELETE user and task

--- src/index.js
+++ src/index.js
@@ -79,7 +79,22 @@ app.patch('/users/:id', async (req, res) => {
     }
 });
 
+// delete user
+app.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id;
 
+    try {
+        const user = await User.findByIdAndDelete(_id);
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
 
 
 //*** TASKS ***//
@@ -150,7 +165,22 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 });
 
+// delete task
+app.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    
+    try {
+        const task = await Task.findByIdAndDelete(_id);
 
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${ port }`);
