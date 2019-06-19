01f76561f85841a72b904f998063fe6817c29a8f
mezzanine510 <mezzanine510@gmail.com>
2019-06-18 17:04:42 -0700

    Setup Task tests; removed unnecessary test files; created db.js to enable test user to be used across multiple tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,24 +1,9 @@
 const supertest = require('supertest');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
 const app = require('../src/app');
 const User = require('../src/models/user');
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
 
-const userOneId = new mongoose.Types.ObjectId();
-const userOne = {
-    _id: userOneId,
-    name: 'Papi',
-    email: 'papi@chulo.com',
-    password: '12what123',
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
-    }]
-}
-
-beforeEach(async () => {
-    await User.deleteMany();
-    await new User(userOne).save();
-});
+beforeEach(setupDatabase);
 
 test('Should signup a new user', async () => {
     const response = await supertest(app).post('/users').send({
